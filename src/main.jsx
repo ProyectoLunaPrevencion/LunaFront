55fb03025c43b27730c8431755d5409f9ec7f65d
Elena Expósito <elexla97@gmail.com>
2025-02-19 12:45:33 +0100

    Add Radix UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./App.jsx";
 import "./styles/reset.css";
+import AppRoutes from "./routes/AppRoutes.jsx";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Theme>
+        <AppRoutes />
+      </Theme>
+    </BrowserRouter>
+  </React.StrictMode>
 );
