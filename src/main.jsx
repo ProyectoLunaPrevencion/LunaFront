2d3abe5ba79d293547af430b43a4a92fd596c344
Elena Expósito <elexla97@gmail.com>
2025-02-20 09:34:55 +0100

    Change styles - Inicio page

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
 import "./styles/reset.css";
 import AppRoutes from "./routes/AppRoutes.jsx";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Theme>
+      <Theme accentColor="pink">
         <AppRoutes />
       </Theme>
     </BrowserRouter>
--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -1,57 +1,126 @@
 import "./Inicio.css";
 import Flower from "../../components/Flowers/Flower";
-import { Flex, Text, Button } from "@radix-ui/themes";
+import {
+  Section,
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Text,
+  Button,
+  Blockquote,
+} from "@radix-ui/themes";
 import { Link } from "react-router-dom";
+import { Footer } from "../../components/Footer/Footer";
 
 export function Inicio() {
   return (
-    <Flex
-      minWidth="100%"
-      height={{ initial: "100%", md: "100vh" }}
-      align="flex-start"
-      justify="center"
-      direction={{ initial: "column", md: "row" }}
-    >
-      <Flex
-        direction="column"
-        align="center"
-        justify="center"
-        gap="6"
-        width={{ initial: "100%", md: "40%" }}
-        p="6"
-      >
-        <Flower />
-        <Flex direction="column" textAlign="start" gap="3" align="start">
-          <Text color="pink" weight="bold" size="6" className="blockquote">
-            “Cuando la conversación con un otro se vuelve refugio. Eso, también
-            es estar en casa.”
-          </Text>
-          <Text color="orange" size="4" className="cite">
-            Ada Luz Márquez
-          </Text>
+    <>
+      <Section p="0">
+        <Flex
+          minWidth="100%"
+          height={{ initial: "100%", md: "100vh" }}
+          align="center"
+          justify="center"
+          direction={{ initial: "column-reverse", md: "row" }}
+        >
+          <Flex
+            direction="column"
+            align="center"
+            justify="center"
+            gap="6"
+            width={{ initial: "100%", md: "40%" }}
+            p="7"
+          >
+            <Flower />
+            <Flex direction="column" textAlign="start" gap="3" align="start">
+              <Blockquote color="pink" size="5" italic>
+                “Cuando la conversación con un otro se vuelve refugio. Eso,
+                también es estar en casa.” <cite>Ada Luz Márquez</cite>
+              </Blockquote>
+            </Flex>
+          </Flex>
+          <Flex
+            className="right-welcome"
+            width="100%"
+            height="100%"
+            align="center"
+            justify="center"
+            p={{ initial: "7", sm: "9" }}
+          >
+            <Box
+              maxWidth={{ initial: "90%", sm: "65rem" }}
+              style={{
+                background: "var(--pink-1)",
+                borderRadius: "1rem",
+                boxShadow: "var(--shadow-6)",
+                width: "100%",
+              }}
+            >
+              <Container>
+                <Flex
+                  direction="column"
+                  gap={{ initial: "4", sm: "9" }}
+                  align="center"
+                  justify="center"
+                  p={{ initial: "5", sm: "9" }}
+                >
+                  <Heading
+                    as="h1"
+                    size={{ initial: "8", sm: "9" }}
+                    align="center"
+                    color="pink"
+                    weight="bold"
+                  >
+                    App Luna, tu espacio seguro
+                  </Heading>
+                  <Flex
+                    direction="column"
+                    align="center"
+                    gap={{ initial: "3", sm: "4" }}
+                  >
+                    <Text
+                      as="p"
+                      size={{ initial: "3", sm: "5" }}
+                      align="center"
+                    >
+                      ¿Te sientes triste continuamente?
+                    </Text>
+                    <Text
+                      as="p"
+                      size={{ initial: "3", sm: "5" }}
+                      align="center"
+                    >
+                      ¿Crees que estás recibiendo maltrato por parte de alguna
+                      persona?
+                    </Text>
+                    <Text
+                      as="p"
+                      size={{ initial: "3", sm: "5" }}
+                      align="center"
+                    >
+                      ¿Tienes pensamientos negativos recurrentes?
+                    </Text>
+                  </Flex>
+                  <Text
+                    as="p"
+                    size={{ initial: "5", sm: "6" }}
+                    align="center"
+                    weight="bold"
+                  >
+                    Aquí estamos para ayudarte
+                  </Text>
+
+                  <Button asChild size="4">
+                    <Link to="/registro">Regístrate</Link>
+                  </Button>
+                </Flex>
+              </Container>
+            </Box>
+          </Flex>
         </Flex>
-      </Flex>
-      <Flex
-        direction="column"
-        gap="8"
-        align="center"
-        justify="center"
-        className="right-welcome"
-        width="100%"
-        p="6"
-      >
-        <Flex direction="column" align="center" gap="6">
-          <Text size="6">¿Te sientes triste continuamente?</Text>
-          <Text size="6">
-            ¿Crees que estás recibiendo maltrato por parte de alguna persona?
-          </Text>
-          <Text size="6">¿Tienes pensamientos negativos recurrentes?</Text>
-          <Text size="6">Aquí estamos para ayudarte</Text>
-        </Flex>
-        <Button asChild variant="soft" size="3" className="login-button">
-          <Link to="/registro">Regístrate</Link>
-        </Button>
-      </Flex>
-    </Flex>
+      </Section>
+      <Footer />
+    </>
   );
 }
