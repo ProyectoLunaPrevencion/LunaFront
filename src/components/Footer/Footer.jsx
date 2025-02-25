fe7d36eb1be0a10555cb32ff756909162149fac5
Elena Expósito <elexla97@gmail.com>
2025-02-25 08:37:33 +0100

    WIP - Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import { Flex, Text } from "@radix-ui/themes";
+import "./Footer.css";
+
+export function Footer() {
+  return (
+    <Flex className="footer">
+      <Text>Hola</Text>
+      <Text>Adiós</Text>
+    </Flex>
+  );
+}
