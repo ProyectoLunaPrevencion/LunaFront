f6df0f5feccb3e0a4f857cb86cc8af583288ebc9
Daniel Ronda <90683193+DaniRonda@users.noreply.github.com>
2025-03-04 11:55:07 +0100

    fixed login, now with header

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -16,9 +16,8 @@ import { useNavigate } from "react-router-dom";
 import { logIn as loginService } from "../../services/authService";
 import { isAxiosError } from "axios";
 import { toast } from "react-hot-toast";
-
+import { Header } from "../../components/Header/Header.jsx";
 import { jwtDecode } from "jwt-decode";
-
 
 export function Login() {
   const {
@@ -34,24 +33,23 @@ export function Login() {
       const response = await loginService(data);
       console.log("Respuesta del backend:", response);
   
-      const token = response?.token; //  Aseguramos que response exista
-      console.log("Token recibido:", token); //  Verificamos el token
-  
+      const token = response?.token;
+      console.log("Token recibido:", token);
+
       if (!token) {
         throw new Error("Token no recibido");
       }
-  
+
       localStorage.setItem("token", token);
-  
+
       const decoded = jwtDecode(token);
       console.log("Token decodificado:", decoded);
-  
+
       const userRole = decoded.rol; 
       toast.success("¡Inicio de sesión exitoso!");
-  
+
       if (userRole === "ESTUDIANTE") {
         navigate("/mi-refugio");
-      
       } else {
         navigate("/");
       }
@@ -65,103 +63,103 @@ export function Login() {
       }
     }
   };
-    
 
   return (
-    <Section p="0">
-      <Flex
-        width="100%"
-        height={{ initial: "100%", lg: "100vh" }}
-        align="center"
-        justify="center"
-        direction={{ initial: "column", lg: "row" }}
-      >
-        <Box width="100%" p={{ initial: "5", xs: "7" }} maxWidth="35rem">
-          <Box
-            width="100%"
-            p="6"
-            style={{
-              background: "var(--pink-1)",
-              borderRadius: "1rem",
-              boxShadow: "var(--shadow-6)",
-              width: "100%",
-            }}
-          >
-            <Container>
-              <Flex
-                direction="column"
-                gap={{ initial: "4", sm: "7" }}
-                align="center"
-                justify="center"
-                width="100%"
-              >
-                <Heading
-                  as="h1"
-                  size={{ initial: "7", lg: "8", xl: "9" }}
+    <>
+      <Header />
+      <Section p="0">
+        <Flex
+          width="100%"
+          height={{ initial: "100%", lg: "100vh" }}
+          align="center"
+          justify="center"
+          direction={{ initial: "column", lg: "row" }}
+        >
+          <Box width="100%" p={{ initial: "5", xs: "7" }} maxWidth="35rem">
+            <Box
+              width="100%"
+              p="6"
+              style={{
+                background: "var(--pink-1)",
+                borderRadius: "1rem",
+                boxShadow: "var(--shadow-6)",
+                width: "100%",
+              }}
+            >
+              <Container>
+                <Flex
+                  direction="column"
+                  gap={{ initial: "4", sm: "7" }}
                   align="center"
-                  color="pink"
-                  weight="bold"
+                  justify="center"
+                  width="100%"
                 >
-                  Inicia sesión
-                </Heading>
-                <Flex align="center" gap={{ initial: "3", sm: "4" }}>
-                  <form onSubmit={handleSubmit(onSubmit)}>
-                    <Flex direction="column" gap={{ initial: "4", xs: "5" }}>
-                      <Input
-                        id="email"
-                        placeholder="Email"
-                        title="Email"
-                        registerProps={register("email", {
-                          required: "El email es obligatorio",
-                          pattern: {
-                            value:
-                              /^[a-zA-Z0-9._%+-]+@colegiovirgendelcarmen\.com$/,
-                            message:
-                              "El email debe pertenecer a @colegiovirgendelcarmen.com",
-                          },
-                        })}
-                        errorMessage={errors.email?.message}
-                        Icon={EnvelopeClosedIcon}
-                      />
-                      <Input
-                        id="password"
-                        placeholder="Contraseña"
-                        title="Contraseña"
-                        registerProps={register("password", {
-                          required: "La contraseña es obligatoria",
-                        })}
-                        errorMessage={errors.password?.message}
-                        Icon={LockClosedIcon}
-                        type="password"
-                      />
-                      <Button
-                        size={{ initial: "3", lg: "4", xl: "5" }}
-                        type="submit"
-                      >
-                        Iniciar sesión
-                      </Button>
-                      <Text
-                        as="p"
-                        align="center"
-                        size={{ initial: "1", lg: "1", xl: "2" }}
-                      >
-                        ¿Aún no tienes cuenta?{" "}
-                        <Link href="/registro" weight="bold" underline="always">
-                          Regístrate
-                        </Link>
-                      </Text>
-                    </Flex>
-                  </form>
+                  <Heading
+                    as="h1"
+                    size={{ initial: "7", lg: "8", xl: "9" }}
+                    align="center"
+                    color="pink"
+                    weight="bold"
+                  >
+                    Inicia sesión
+                  </Heading>
+                  <Flex align="center" gap={{ initial: "3", sm: "4" }}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                      <Flex direction="column" gap={{ initial: "4", xs: "5" }}>
+                        <Input
+                          id="email"
+                          placeholder="Email"
+                          title="Email"
+                          registerProps={register("email", {
+                            required: "El email es obligatorio",
+                            pattern: {
+                              value: /^[a-zA-Z0-9._%+-]+@colegiovirgendelcarmen\.com$/,
+                              message: "El email debe pertenecer a @colegiovirgendelcarmen.com",
+                            },
+                          })}
+                          errorMessage={errors.email?.message}
+                          Icon={EnvelopeClosedIcon}
+                        />
+                        <Input
+                          id="password"
+                          placeholder="Contraseña"
+                          title="Contraseña"
+                          registerProps={register("password", {
+                            required: "La contraseña es obligatoria",
+                          })}
+                          errorMessage={errors.password?.message}
+                          Icon={LockClosedIcon}
+                          type="password"
+                        />
+                        <Button
+                          size={{ initial: "3", lg: "4", xl: "5" }}
+                          type="submit"
+                        >
+                          Iniciar sesión
+                        </Button>
+                        <Text
+                          as="p"
+                          align="center"
+                          size={{ initial: "1", lg: "1", xl: "2" }}
+                        >
+                          ¿Aún no tienes cuenta?{" "}
+                          <Link href="/registro" weight="bold" underline="always">
+                            Regístrate
+                          </Link>
+                        </Text>
+                      </Flex>
+                    </form>
+                  </Flex>
                 </Flex>
-              </Flex>
-            </Container>
+              </Container>
+            </Box>
           </Box>
-        </Box>
 
-        <Box width={{ initial: "100%", lg: "50%" }}>
-          <img src={signupImage} alt="Inicio de sesión" />
-        </Box>
-      </Flex>
-    </Section>
+          <Box width={{ initial: "100%", lg: "50%" }}>
+            <img src={signupImage} alt="Inicio de sesión" />
+          </Box>
+        </Flex>
+      </Section>
+    </>
   );
 }
