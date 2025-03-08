1a7368d9440348fbf5c9c1ba8c105f520b77bf19
Elena Expósito <elexla97@gmail.com>
2025-03-08 12:48:22 +0100

    Fix login problem

--- src/routes/guards/DashboardGuard.jsx
+++ src/routes/guards/DashboardGuard.jsx
@@ -2,13 +2,19 @@ import PropTypes from "prop-types";
 import { useCurrentUserQuery } from "../../hooks/queries/useCurrentUserQuery";
 import { Spinner } from "@radix-ui/themes";
 import { Navigate, useLocation } from "react-router";
+import { cookies, SESSION_COOKIE } from "../../utils/cookieManager";
 
 const ALLOWED_ROLES = ["PROFESOR", "ORIENTACION"];
 const DASHBOARD_COMMON_PATHNAMES = ["", "/informacion", "/ajustes", "/pizarra"];
 
 export function DashboardGuard({ children }) {
+  const sessionCookie = cookies.get(SESSION_COOKIE);
   const { data: currentUser, isLoading } = useCurrentUserQuery();
   const { pathname } = useLocation();
+
+  if (!sessionCookie) {
+    return children;
+  }
 
   if (isLoading || !currentUser) {
     return <Spinner />;
